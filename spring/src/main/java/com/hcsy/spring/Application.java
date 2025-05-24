6df5d527d6fe44159442521bce3f115d4200101c
hongch666 <3070872194@qq.com>
2025-05-24 17:04:45 +0800

    启动类修改

--- spring/src/main/java/com/hcsy/spring/Application.java
+++ spring/src/main/java/com/hcsy/spring/Application.java
@@ -4,7 +4,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.ConfigurableApplicationContext;
 
 @Slf4j
 @EnableFeignClients()
@@ -12,7 +11,7 @@ import org.springframework.context.ConfigurableApplicationContext;
 public class Application {
 
     public static void main(String[] args) {
-        ConfigurableApplicationContext ctx = SpringApplication.run(Application.class, args);
+        SpringApplication.run(Application.class, args);
     }
 
 }
