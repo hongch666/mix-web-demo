9250bad0bba8fd64953c43dbe34ea684ca3a6399
hongch666 <3070872194@qq.com>
2025-06-01 00:04:12 +0800

    移除部分注解

--- spring/src/main/java/com/hcsy/spring/Application.java
+++ spring/src/main/java/com/hcsy/spring/Application.java
@@ -7,7 +7,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@Slf4j
 @EnableFeignClients()
 @SpringBootApplication
 @MapperScan(basePackages = "com.hcsy.spring.mapper")
