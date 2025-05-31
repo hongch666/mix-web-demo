52e71742c8986937f31a98e91d6169737a4f9e1a
hongch666 <3070872194@qq.com>
2025-05-31 18:05:26 +0800

    新增spring模块的mp使用

---
+++ spring/src/main/java/com/hcsy/spring/po/User.java
@@ -0,0 +1,16 @@
+package com.hcsy.spring.po;
+
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
+
+@Data
+@TableName("user") // 对应数据库中的表名
+public class User {
+    @TableId
+    private Long id;
+    private String password;
+    private String name;
+    private Integer age;
+    private String email;
+}
