815ecc8f9df51cb4c1b2f6f981c27bf5d36c0db7
hongch666 <3070872194@qq.com>
2025-06-02 01:18:47 +0800

    添加Spring部分Swagger文档

--- spring/src/main/java/com/hcsy/spring/controller/UserController.java
+++ spring/src/main/java/com/hcsy/spring/controller/UserController.java
@@ -2,6 +2,10 @@ package com.hcsy.spring.controller;
 
 import com.hcsy.spring.po.User;
 import com.hcsy.spring.service.UserService;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -9,16 +13,20 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/users")
+@Tag(name = "用户模块", description = "用户相关接口")
 public class UserController {
+
     @Autowired
     private UserService userService;
 
     @GetMapping
+    @Operation(summary = "获取用户信息", description = "获取用户信息列表")
     public List<User> listUsers() {
         return userService.list();
     }
 
     @PostMapping
+    @Operation(summary = "新增用户", description = "通过请求体创建用户信息")
     public boolean addUser(@RequestBody User user) {
         return userService.save(user);
     }
