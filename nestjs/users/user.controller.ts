8987f516922786dced4cb8b18b6e5a521336644d
hongch666 <3070872194@qq.com>
2025-06-02 01:36:12 +0800

    添加NestJS部分Swagger文档

--- nestjs/users/user.controller.ts
+++ nestjs/users/user.controller.ts
@@ -2,17 +2,21 @@
 import { Controller, Get, Post, Body } from '@nestjs/common';
 import { UserService } from './user.service';
 import { User } from './entity/user.entity';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
 @Controller('users')
+@ApiTags('测试模块')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Get()
+  @ApiOperation({ summary: '获取用户信息', description: '获取用户信息列表' })
   findAll(): Promise<User[]> {
     return this.userService.findAll();
   }
 
   @Post()
+  @ApiOperation({ summary: '新增用户', description: '通过请求体创建用户信息' })
   create(@Body() userData: Partial<User>): Promise<User> {
     return this.userService.create(userData);
   }
