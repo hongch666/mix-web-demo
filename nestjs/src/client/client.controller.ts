a745f07c7b6faa6eba0053046434027413297b3c
hongch666 <3070872194@qq.com>
2025-06-02 01:36:12 +0800

    添加NestJS部分Swagger文档

--- nestjs/src/client/client.controller.ts
+++ nestjs/src/client/client.controller.ts
@@ -1,31 +1,36 @@
 import { Controller, Get, Post, Query, Param, Body, Req } from '@nestjs/common';
 import { NacosService } from '../nacos/nacos.service';
-import { success, error } from "../utils/response";
+import { success, error } from '../utils/response';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
 @Controller('api_nestjs')
+@ApiTags('用户模块')
 export class ClientController {
-    constructor(private readonly nacosService: NacosService) {}
+  constructor(private readonly nacosService: NacosService) {}
 
-    @Get('nestjs')
-    async getNestjs():Promise<any>{
-        return success("Hello,I am Nest.js!")
-    }
+  @Get('nestjs')
+  @ApiOperation({ summary: 'NestJS自己的测试', description: '输出欢迎信息' })
+  async getNestjs(): Promise<any> {
+    return success('Hello,I am Nest.js!');
+  }
 
-    @Get('spring')
-    async getSpring():Promise<any>{
-        return this.nacosService.call({
-            serviceName: 'spring',
-            method: 'GET',
-            path: '/api_spring/spring'
-        })
-    }
+  @Get('spring')
+  @ApiOperation({ summary: '调用Spring的测试', description: '输出欢迎信息' })
+  async getSpring(): Promise<any> {
+    return this.nacosService.call({
+      serviceName: 'spring',
+      method: 'GET',
+      path: '/api_spring/spring',
+    });
+  }
 
-    @Get('gin')
-    async getGin():Promise<any>{
-        return this.nacosService.call({
-            serviceName: 'gin',
-            method: 'GET',
-            path: '/api_gin/gin'
-        })
-    }
+  @Get('gin')
+  @ApiOperation({ summary: '调用Gin的测试', description: '输出欢迎信息' })
+  async getGin(): Promise<any> {
+    return this.nacosService.call({
+      serviceName: 'gin',
+      method: 'GET',
+      path: '/api_gin/gin',
+    });
+  }
 }
--- nestjs/src/main.ts
+++ nestjs/src/main.ts
@@ -2,11 +2,22 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { AllExceptionsFilter } from './filters/all-exception.filter';
 import { ConfigService } from '@nestjs/config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // Swagger 配置
+  const config = new DocumentBuilder()
+    .setTitle('NestJS部分的Swagger文档集成')
+    .setDescription('这是demo项目的NestJS部分的Swagger文档集成')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document);
   // 注册全局异常过滤器
   app.useGlobalFilters(new AllExceptionsFilter());
+
   // 读取yaml文件中的端口
   const configService = app.get(ConfigService);
   const port = configService.get<number>('server.port') || 3000;
