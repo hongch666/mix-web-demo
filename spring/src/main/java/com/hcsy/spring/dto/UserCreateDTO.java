21ad32c9a3cbe5cebf27019c16808144489dd087
hongch666 <3070872194@qq.com>
2025-06-02 18:15:11 +0800

    修改NestJS, 新增Spring部分DTO校验

--- spring/src/main/java/com/hcsy/spring/dto/UserCreateDTO.java
+++ spring/src/main/java/com/hcsy/spring/dto/UserCreateDTO.java
@@ -3,17 +3,27 @@ package com.hcsy.spring.dto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import jakarta.validation.constraints.*;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class UserCreateDTO {
 
+    @NotBlank(message = "密码不能为空")
+    @Size(min = 3, max = 20, message = "密码长度必须在3到20个字符之间")
     private String password;
 
+    @NotBlank(message = "用户名不能为空")
+    @Size(min = 3, max = 20, message = "用户名长度必须在3到20个字符之间")
     private String name;
 
+    @NotNull(message = "年龄不能为空")
+    @Min(value = 0, message = "年龄不能小于0岁")
+    @Max(value = 150, message = "年龄不能大于150岁")
     private Integer age;
 
+    @NotBlank(message = "邮箱不能为空")
+    @Email(message = "邮箱格式不正确")
     private String email;
 }
--- spring/src/main/java/com/hcsy/spring/dto/UserUpdateDTO.java
+++ spring/src/main/java/com/hcsy/spring/dto/UserUpdateDTO.java
@@ -3,10 +3,14 @@ package com.hcsy.spring.dto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import jakarta.validation.constraints.*;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class UserUpdateDTO extends UserCreateDTO {
+
+    @NotNull(message = "id不能为空")
+    @Min(value = 0, message = "id不能小于0")
     private Integer id;
 }
