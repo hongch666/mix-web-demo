59e5c0f07c8b4c2e1eed1e653aa4f059e133777d
hongch666 <3070872194@qq.com>
2025-06-02 18:23:10 +0800

    修改Spring部分全局异常捕获器

--- spring/src/main/java/com/hcsy/spring/exception/GlobalExceptionHandler.java
+++ spring/src/main/java/com/hcsy/spring/exception/GlobalExceptionHandler.java
@@ -1,14 +1,18 @@
 package com.hcsy.spring.exception;
 
 import com.hcsy.spring.po.Result;
+
+import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler {
     @ExceptionHandler(Exception.class)
     public Result ex(Exception ex) {
-        ex.printStackTrace();
+        log.error("捕获到异常: " + ex.getMessage());
         return Result.error(ex.getMessage());
     }
 }
