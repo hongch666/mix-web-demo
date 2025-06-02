270ab325b7a1e98c848dfd094737dffcd4b4b322
hongch666 <3070872194@qq.com>
2025-06-02 18:29:49 +0800

    新增Gin部分DTO校验

--- gin/dto/UserCreateDTO.go
+++ gin/dto/UserCreateDTO.go
@@ -1,8 +1,8 @@
 package dto
 
 type UserCreateDTO struct {
-	Password string `json:"password"`
-	Name     string `json:"name"`
-	Age      int    `json:"age"`
-	Email    string `json:"email"`
+	Password string `json:"password" binding:"required,min=3,max=20"`
+	Name     string `json:"name" binding:"required,min=3,max=20"`
+	Age      int    `json:"age" binding:"required,min=0,max=150"`
+	Email    string `json:"email" binding:"required,email"`
 }
--- gin/dto/UserUpdateDTO.go
+++ gin/dto/UserUpdateDTO.go
@@ -1,6 +1,6 @@
 package dto
 
 type UserUpdateDTO struct {
-	ID int `json:"id"`
+	ID int `json:"id" binding:"required,min=0"`
 	UserCreateDTO
 }
