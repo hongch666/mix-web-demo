41e43c6a9b3d8efc463d0ebd56209b6e0e207762
hongch666 <3070872194@qq.com>
2025-06-02 18:55:29 +0800

    修改Spring模块不需要的导包

--- spring/src/main/java/com/hcsy/spring/dto/UserUpdateDTO.java
+++ spring/src/main/java/com/hcsy/spring/dto/UserUpdateDTO.java
@@ -2,10 +2,12 @@ package com.hcsy.spring.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import jakarta.validation.constraints.*;
 
 @Data
+@EqualsAndHashCode(callSuper = false)
 @AllArgsConstructor
 @NoArgsConstructor
 public class UserUpdateDTO extends UserCreateDTO {
