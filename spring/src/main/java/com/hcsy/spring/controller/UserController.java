25d8efa72cdcbc3bcb6b28525bf7ae448fbbb442
hongch666 <3070872194@qq.com>
2025-06-02 16:07:38 +0800

    新增Spring部分的用户CRUD

--- spring/src/main/java/com/hcsy/spring/controller/UserController.java
+++ spring/src/main/java/com/hcsy/spring/controller/UserController.java
@@ -1,33 +1,59 @@
 package com.hcsy.spring.controller;
 
+import com.hcsy.spring.dto.UserCreateDTO;
+import com.hcsy.spring.dto.UserUpdateDTO;
+import com.hcsy.spring.po.Result;
 import com.hcsy.spring.po.User;
 import com.hcsy.spring.service.UserService;
 
+import cn.hutool.core.bean.BeanUtil;
+import feign.Param;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/users")
+@RequiredArgsConstructor
 @Tag(name = "用户模块", description = "用户相关接口")
 public class UserController {
-
-    @Autowired
-    private UserService userService;
+    private final UserService userService;
 
     @GetMapping
     @Operation(summary = "获取用户信息", description = "获取用户信息列表")
-    public List<User> listUsers() {
-        return userService.list();
+    public Result listUsers() {
+        return Result.success(userService.list());
     }
 
     @PostMapping
     @Operation(summary = "新增用户", description = "通过请求体创建用户信息")
-    public boolean addUser(@RequestBody User user) {
-        return userService.save(user);
+    public Result addUser(@RequestBody UserCreateDTO userDto) {
+        User user = BeanUtil.copyProperties(userDto, User.class);
+        userService.save(user);
+        return Result.success();
+    }
+
+    @DeleteMapping("/{id}")
+    @Operation(summary = "删除用户", description = "根据id删除用户")
+    public Result deleteUser(@Param Long id) {
+        userService.removeById(id);
+        return Result.success();
+    }
+
+    @GetMapping("/{id}")
+    @Operation(summary = "查询用户", description = "根据id查询用户")
+    public Result getUserById(@Param Long id) {
+        User user = userService.getById(id);
+        return Result.success(user);
+    }
+
+    @PutMapping
+    @Operation(summary = "修改用户", description = "通过请求体修改用户信息")
+    public Result updateUser(@RequestBody UserUpdateDTO userDto) {
+        User user = BeanUtil.copyProperties(userDto, User.class);
+        userService.updateById(user);
+        return Result.success();
     }
 }
