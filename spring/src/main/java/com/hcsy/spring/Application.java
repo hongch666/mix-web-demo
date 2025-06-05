6939c6604b140e4e9e8fd54037a284c430d5f900
hongch666 <3070872194@qq.com>
2025-06-05 23:26:52 +0800

    TDOD修改

--- spring/src/main/java/com/hcsy/spring/Application.java
+++ spring/src/main/java/com/hcsy/spring/Application.java
@@ -5,6 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+// TODO: 增加文章模块的增删改，查询使用ES的搜索功能
 // TODO: 增加对Redis的调用
 // TODO: 使用Redis存储用户状态（在线/离线）
 // TODO: 增加分页查询的处理和对应的类
