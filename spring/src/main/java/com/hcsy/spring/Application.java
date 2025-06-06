25a5876c5b87fa2030ab954c518c8f1de3750348
hongch666 <3070872194@qq.com>
2025-06-06 20:05:26 +0800

    TODO修改

--- spring/src/main/java/com/hcsy/spring/Application.java
+++ spring/src/main/java/com/hcsy/spring/Application.java
@@ -5,12 +5,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-// TODO: 文章查询使用ES的搜索功能
-// TODO: 文章增删改后调用Gin部分的同步代码（要有事务注解）
-// TODO: 文章增删改查后使用NestJS部分的创建日志功能增加日志
 // TODO: 增加对Redis的调用
 // TODO: 使用Redis存储用户状态（在线/离线）
 // TODO: 增加分页查询的处理和对应的类
+// TODO: 文章查询使用ES的搜索功能
+// TODO: 文章增删改后调用Gin部分的同步代码（要有事务注解）
+// TODO: 文章增删改查后使用NestJS部分的创建日志功能增加日志
+// TODO: 日志创建部分不使用feign客户端调用，而是将日志内容生产到RabbitMQ中
+// TODO: 增加发布文章功能（修改发布状态），注意要同时同步到ES中,并且发送消息到RabbitMQ中
+// TODO: 使用定时任务，每天0点检查是否有文章未发布，自动发布（调用上述逻辑）
 
 @EnableFeignClients()
 @SpringBootApplication
