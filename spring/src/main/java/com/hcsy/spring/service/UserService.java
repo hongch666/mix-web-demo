7ce32688d7c82647373c19bb89f21ea082b4f732
hongch666 <3070872194@qq.com>
2025-06-07 13:35:19 +0800

    Spring模块增加Redis存储用户状态

--- spring/src/main/java/com/hcsy/spring/service/UserService.java
+++ spring/src/main/java/com/hcsy/spring/service/UserService.java
@@ -1,11 +1,9 @@
 package com.hcsy.spring.service;
 
-import com.hcsy.spring.po.Article;
 import com.hcsy.spring.po.User;
-
-import java.util.List;
 
 import com.baomidou.mybatisplus.extension.service.IService;
 
 public interface UserService extends IService<User> {
+    void saveUserAndStatus(User user);
 }
--- spring/src/main/java/com/hcsy/spring/service/impl/UserServiceImpl.java
+++ spring/src/main/java/com/hcsy/spring/service/impl/UserServiceImpl.java
@@ -3,9 +3,23 @@ package com.hcsy.spring.service.impl;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.hcsy.spring.po.User;
 import com.hcsy.spring.service.UserService;
+import com.hcsy.spring.utils.RedisUtil;
+
+import lombok.RequiredArgsConstructor;
+
 import com.hcsy.spring.mapper.UserMapper;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@RequiredArgsConstructor
+@Transactional
 public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements UserService {
+    private final UserMapper userMapper;
+    private final RedisUtil redisUtil;
+
+    public void saveUserAndStatus(User user) {
+        userMapper.insert(user);
+        redisUtil.set("user:status:" + user.getId(), "0");
+    }
 }
