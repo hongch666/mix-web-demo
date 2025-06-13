1cec0e66b4d946a8217446253cc4fd206d1c17fd
hongch666 <3070872194@qq.com>
2025-06-14 00:35:40 +0800

    FastAPI部分增加统计浏览量前10的功能

--- fastapi/main.py
+++ fastapi/main.py
@@ -1,10 +1,10 @@
 from fastapi import FastAPI
 from controller.testController import router as test_router
+from controller.analyzeController import router as analyze_router
 import uvicorn
 from config.nacos import start_nacos
 from config.config import load_config
 
-# TODO: 对数据库的文章浏览量进行数据分析，获取前十名浏览量最高的文章
 # TODO: 对日志中的搜索部分进行数据分析，根据搜索关键字进行词云图的生成（保存到本地）
 # TODO: 使用上述数据进行机器学习分析，预测后续的文章应当使用的主题
 
@@ -18,6 +18,7 @@ app = FastAPI(
     version="1.0.0")
 
 app.include_router(test_router)
+app.include_router(analyze_router)
 
 @app.on_event("startup")
 def startup_event():
