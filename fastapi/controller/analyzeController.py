dc69b937a4f6e10b10d2593391cd5b455f7208ae
hongch666 <3070872194@qq.com>
2025-06-14 01:26:27 +0800

    FastAPI部分增加生成词云图功能

--- fastapi/controller/analyzeController.py
+++ fastapi/controller/analyzeController.py
@@ -4,8 +4,7 @@ from po.listResponse import ListResponse
 from service.analyzeService import get_top10_articles_service
 from po.article import Article
 from config.mysql import SessionLocal,get_db
-
-from client.client import call_remote_service
+from service.analyzeService import generate_wordcloud,get_keywords_dic
 from utils.response import success
 
 router = APIRouter(
@@ -17,3 +16,8 @@ router = APIRouter(
 def get_top10_articles(db: Session = Depends(get_db)):
     articles = get_top10_articles_service(db)
     return success(ListResponse(total=len(articles), list=articles))
+
+@router.get("/wordcloud")
+def get_wordcloud():
+    generate_wordcloud(get_keywords_dic())
+    return success()
--- fastapi/service/analyzeService.py
+++ fastapi/service/analyzeService.py
@@ -1,9 +1,42 @@
+import logging
 from sqlalchemy.orm import Session
 from fastapi import Depends
 
 from config.mysql import get_db
 from po.article import Article
+from config.mongodb import db as mongo_db
+from config.logger import logger
+from wordcloud import WordCloud
 
 def get_top10_articles_service(db: Session = Depends(get_db)):
     articles = db.query(Article).order_by(Article.views.desc()).limit(10).all()
     return articles
+
+def get_keywords_dic():
+    logs = mongo_db["articlelogs"]
+    cursor = logs.find({"action": "search"})
+    all_keywords = []
+    for log in cursor:
+        content = log.get('content', {})
+        if 'Keyword' in content:
+            if content['Keyword'] == "":
+                continue
+            all_keywords.append(content['Keyword'])
+    keywords_dic = {}
+    for keyword in all_keywords:
+        if keyword in keywords_dic:
+            keywords_dic[keyword] += 1
+        else:
+            keywords_dic[keyword] = 1
+    return keywords_dic
+
+def generate_wordcloud(keywords_dic):
+    wc = WordCloud(
+        font_path="/usr/share/fonts/opentype/noto/NotoSansCJK-Regular.ttc", 
+        width=800,
+        height=400,
+        background_color="white"
+    )
+    wc.generate_from_frequencies(keywords_dic)
+    wc.to_file("fastapi/pic/search_keywords_wordcloud.png")
+    logger.info("词云图生成成功，保存为 search_keywords_wordcloud.png")
