af74b7fcfdfe42a79c7117c0beadfa36be687aba
hongch666 <3070872194@qq.com>
2025-07-24 14:07:49 +0800

    Gin部分搜索文章功能确保仅在有关键字时发送消息

--- gin/api/service/search_service.go
+++ gin/api/service/search_service.go
@@ -14,22 +14,25 @@ func SearchArticles(ctx context.Context, searchDTO dto.ArticleSearchDTO) po.Sear
 	data, total := mapper.SearchArticle(ctx, searchDTO)
 	// 读取用户id
 	userID, _ := ctx.Value(ctxkey.UserIDKey).(int64)
-	// 发送消息
-	msg := map[string]interface{}{
-		"action":  "search",
-		"user_id": userID,
-		"content": searchDTO,
-		"msg":     "发起了文章搜索",
-	}
-	// 2. 转成 JSON 字符串
-	jsonBytes, err := json.Marshal(msg)
-	if err != nil {
-		panic(err.Error())
-	}
-	// 3. 发送消息
-	err = config.RabbitMQ.Send("log-queue", string(jsonBytes))
-	if err != nil {
-		panic(err.Error())
+	// 如果搜索关键字为空，就不发送消息
+	if searchDTO.Keyword != "" {
+		// 发送消息
+		msg := map[string]interface{}{
+			"action":  "search",
+			"user_id": userID,
+			"content": searchDTO,
+			"msg":     "发起了文章搜索",
+		}
+		// 转成 JSON 字符串
+		jsonBytes, err := json.Marshal(msg)
+		if err != nil {
+			panic(err.Error())
+		}
+		// 发送消息
+		err = config.RabbitMQ.Send("log-queue", string(jsonBytes))
+		if err != nil {
+			panic(err.Error())
+		}
 	}
 	return po.SearchResult{
 		Total: total,
