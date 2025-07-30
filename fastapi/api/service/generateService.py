91399642a583a92d6dde8cd55b23d5d942a89f42
hongch666 <3070872194@qq.com>
2025-07-30 15:42:04 +0800

    FastAPI部分获取关键字增加去除markdown格式符号

--- fastapi/api/service/generateService.py
+++ fastapi/api/service/generateService.py
@@ -1,4 +1,5 @@
 import jieba.analyse
+import re
 
 def extract_tags(text: str, topK: int = 5) -> str:
     """
@@ -7,5 +8,8 @@ def extract_tags(text: str, topK: int = 5) -> str:
     :param topK: 返回关键词数量
     :return: 关键词列表
     """
+    # 去除markdown格式符号
+    text = re.sub(r'(```[\s\S]*?```|`[^`]*`|\!\[[^\]]*\]\([^\)]*\)|\[[^\]]*\]\([^\)]*\)|[#>*_~\-\+\=\[\]`]|\d+\.|\n)', ' ', text)
+    text = re.sub(r'\s+', ' ', text).strip()
     tags: list[str] = jieba.analyse.extract_tags(text, topK=topK)
     return ",".join(tags)
