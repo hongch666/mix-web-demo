ffb39f33f810a3d12b38770d7c4332e64208bd0f
hongch666 <3070872194@qq.com>
2025-08-01 11:54:44 +0800

    FastAPI部分重构应用启动逻辑，移除冗余的启动事件处理

--- fastapi/common/handler/exception_handlers.py
+++ fastapi/common/handler/exception_handlers.py
@@ -1,10 +1,11 @@
 from fastapi import Request
 from fastapi.responses import JSONResponse
 from fastapi.responses import Response
-from common.utils import fail
+from common.utils import fail,fileLogger
 from typing import Any
 
 async def global_exception_handler(request: Request, exc: Exception) -> Response:
+    fileLogger.error(f"请求路径: {request.url}，错误信息: {str(exc)}")
     return JSONResponse(
         status_code=500,
         content=fail(str(exc))
--- fastapi/main.py
+++ fastapi/main.py
@@ -7,16 +7,27 @@ from common.middleware import ContextMiddleware
 from common.handler import global_exception_handler
 from common.task import start_scheduler
 from typing import Dict, Any
+from contextlib import asynccontextmanager
 
 server_config: Dict[str, Any] = load_config("server")
 IP: str = server_config["ip"]
 PORT: int = server_config["port"]
 
 def create_app() -> FastAPI:
+    @asynccontextmanager
+    async def lifespan(app: FastAPI):
+        start_nacos(port=PORT)
+        logger.info(f"FastAPI应用已启动")
+        logger.info(f"服务地址:http://{IP}:{PORT}")
+        logger.info(f"Swagger文档地址: http://{IP}:{PORT}/docs")
+        logger.info(f"ReDoc文档地址: http://{IP}:{PORT}/redoc")
+        yield
+
     app = FastAPI(
         title="FastAPI部分的Swagger文档集成",
         description="这是demo项目的FastAPI部分的Swagger文档集成",
-        version="1.0.0"
+        version="1.0.0",
+        lifespan=lifespan
     )
     app.add_middleware(ContextMiddleware)
     app.add_exception_handler(Exception, global_exception_handler)
@@ -29,13 +40,6 @@ def create_app() -> FastAPI:
 
 start_scheduler()
 app = create_app()
-@app.on_event("startup")
-def startup_event() -> None:
-    start_nacos(port=PORT)
-    logger.info(f"FastAPI应用已启动")
-    logger.info(f"服务地址:http://{IP}:{PORT}")
-    logger.info(f"Swagger文档地址: http://{IP}:{PORT}/docs")
-    logger.info(f"ReDoc文档地址: http://{IP}:{PORT}/redoc")
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host=IP, port=PORT, reload=True)
