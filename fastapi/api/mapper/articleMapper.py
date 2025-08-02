d9dc6cd1881408ac6a13f2ab916f3230e5e8ef8e
hongch666 <3070872194@qq.com>
2025-08-02 16:55:33 +0800

    FastAPI部分优化获取前10篇文章的查询逻辑

--- fastapi/api/mapper/articleMapper.py
+++ fastapi/api/mapper/articleMapper.py
@@ -5,7 +5,7 @@ from config import get_db
 from entity.po import Article
 
 def get_top10_articles_mapper(db: Session) -> list[Article]:
-    return db.query(Article).limit(10).all()
+    return db.query(Article).order_by(Article.views.desc()).limit(10).all()
 
 def get_all_articles_mapper(db: Session) -> list[Article]:
     return db.query(Article).all()
