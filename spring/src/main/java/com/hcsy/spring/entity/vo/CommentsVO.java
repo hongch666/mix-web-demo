73a937e5dc3a21e2cae2f9601178966310b025c4
hongch666 <3070872194@qq.com>
2025-09-11 17:46:53 +0800

    Spring部分评论修改星级显示

--- spring/src/main/java/com/hcsy/spring/entity/vo/CommentsVO.java
+++ spring/src/main/java/com/hcsy/spring/entity/vo/CommentsVO.java
@@ -6,6 +6,7 @@ import lombok.Data;
 public class CommentsVO {
     private Long id;
     private String content;
+    private Double star;
     private String username;
     private String articleTitle;
     private String createTime;
