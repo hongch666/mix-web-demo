d05d0f851c81611ba2172b7d921854a2d6f1ebf5
hongch666 <3070872194@qq.com>
2025-09-22 17:26:29 +0800

    NestJS部分增加ApiLog日志注解用于接口调用日志记录

--- nestjs/src/api/article/article.controller.ts
+++ nestjs/src/api/article/article.controller.ts
@@ -1,32 +1,17 @@
 import { Controller, Get, Param } from '@nestjs/common';
 import { ArticleService } from './article.service';
 import { ApiOperation, ApiTags } from '@nestjs/swagger';
-import { ClsService } from 'nestjs-cls';
-import { fileLogger } from 'src/common/utils/writeLog';
+import { ApiLog } from 'src/common/decorators/api-log.decorator';
 
 @Controller('article')
 @ApiTags('文章模块')
 export class ArticleController {
-  constructor(
-    private readonly articleService: ArticleService,
-    private readonly cls: ClsService,
-  ) {}
+  constructor(private readonly articleService: ArticleService) {}
 
   @Get('download/:id')
   @ApiOperation({ summary: '下载文章', description: '通过id下载对应文章' })
+  @ApiLog('下载文章')
   async downloadWord(@Param('id') id: number) {
-    const userId = this.cls.get('userId');
-    const username = this.cls.get('username');
-    fileLogger.info(
-      '用户' +
-        userId +
-        ':' +
-        username +
-        ' GET /article/download/:id' +
-        '下载文章\n' +
-        'ID: ' +
-        id,
-    );
     const url = await this.articleService.exportToWordAndSave(id);
     return url;
   }
@@ -36,19 +21,8 @@ export class ArticleController {
     summary: '下载文章Markdown',
     description: '通过id下载对应文章的Markdown并返回OSS链接',
   })
+  @ApiLog('下载文章Markdown')
   async downloadMarkdown(@Param('id') id) {
-    const userId = this.cls.get('userId');
-    const username = this.cls.get('username');
-    fileLogger.info(
-      '用户' +
-        userId +
-        ':' +
-        username +
-        ' GET /article/download-markdown/:id' +
-        '下载文章Markdown\n' +
-        'ID: ' +
-        id,
-    );
     const url = await this.articleService.exportMarkdownAndUpload(Number(id));
     return url;
   }
