fd15759dcf45b74fb230ac279e617388300ca23d
hongch666 <3070872194@qq.com>
2025-09-22 17:45:41 +0800

    FatsAPI部分增加Log日志注解用于接口调用日志记录

--- fastapi/api/controller/analyzeController.py
+++ fastapi/api/controller/analyzeController.py
@@ -1,10 +1,10 @@
 from fastapi import APIRouter, Depends
 from sqlmodel import Session
 from api.service import AnalyzeService, get_analyze_service
-from common.middleware import get_current_user_id, get_current_username
 from entity.po import ListResponse
 from config import get_db
-from common.utils import success,fileLogger
+from common.utils import success
+from common.decorators import log
 from starlette.concurrency import run_in_threadpool
 from typing import Any, Dict, List
 
@@ -14,28 +14,22 @@ router: APIRouter = APIRouter(
 )
 
 @router.get("/top10")
+@log("获取前10篇文章")
 async def get_top10_articles(db: Session = Depends(get_db), analyzeService: AnalyzeService = Depends(get_analyze_service)) -> Any:
-    user_id: str = get_current_user_id() or ""
-    username: str = get_current_username() or ""
-    fileLogger.info("用户" + user_id + ":" + username + " GET /analyze/top10: 获取前10篇文章")
     articles: List[Dict[str, Any]] = await run_in_threadpool(analyzeService.get_top10_articles_service, db)
     return success(ListResponse(total=len(articles), list=articles))
 
 @router.post("/wordcloud")
+@log("生成词云图")
 async def get_wordcloud(analyzeService: AnalyzeService = Depends(get_analyze_service)) -> Any:
-    user_id: str = get_current_user_id() or ""
-    username: str = get_current_username() or ""
-    fileLogger.info("用户" + user_id + ":" + username + " POST /analyze/wordcloud: 生成词云图")
     keywords_dic: Dict[str, int] = await run_in_threadpool(analyzeService.get_keywords_dic)
     await run_in_threadpool(analyzeService.generate_wordcloud, keywords_dic)
     oss_url: str = await run_in_threadpool(analyzeService.upload_wordcloud_to_oss)
     return success(oss_url)
 
 @router.post("/excel")
+@log("获取文章数据Excel")
 async def get_excel(db: Session = Depends(get_db), analyzeService: AnalyzeService = Depends(get_analyze_service)) -> Any:
-    user_id: str = get_current_user_id() or ""
-    username: str = get_current_username() or ""
-    fileLogger.info("用户" + user_id + ":" + username + " POST /analyze/excel: 获取文章数据Excel")
     await run_in_threadpool(analyzeService.export_articles_to_excel, db)
     oss_url: str = await run_in_threadpool(analyzeService.upload_excel_to_oss)
     return success(oss_url)
--- fastapi/api/controller/testController.py
+++ fastapi/api/controller/testController.py
@@ -2,10 +2,10 @@ from fastapi import APIRouter
 from fastapi.responses import JSONResponse
 from typing import Any, Dict
 
+from common.decorators import log
 from common.client import call_remote_service
 from common.task import export_articles_to_csv_and_hive
 from common.utils import success,fail,fileLogger
-from common.middleware import get_current_user_id, get_current_username
 
 router: APIRouter = APIRouter(
     prefix="/api_fastapi",
@@ -14,18 +14,14 @@ router: APIRouter = APIRouter(
 
 # 测试
 @router.get("/fastapi")
+@log("测试FastAPI服务")
 async def testFastapi() -> JSONResponse:
-    user_id: str = get_current_user_id() or ""
-    username: str = get_current_username() or ""
-    fileLogger.info("用户" + user_id + ":" + username + " GET /api_fastapi/fastapi: 测试FastAPI服务")
     return success("Hello, I am FastAPI!")
 
 # 测试Spring服务
 @router.get("/spring")
+@log("测试Spring服务")
 async def testSpring() -> JSONResponse:
-    user_id: str = get_current_user_id() or ""
-    username: str = get_current_username() or ""
-    fileLogger.info("用户" + user_id + ":" + username + " GET /api_fastapi/spring: 测试Spring服务")
     result: Dict[str, Any] = await call_remote_service(
         service_name="spring",
         path="/api_spring/spring",
@@ -36,10 +32,8 @@ async def testSpring() -> JSONResponse:
 
 # 测试Gin服务
 @router.get("/gin")
+@log("测试Gin服务")
 async def testGin() -> JSONResponse:
-    user_id: str = get_current_user_id() or ""
-    username: str = get_current_username() or ""
-    fileLogger.info("用户" + user_id + ":" + username + " GET /api_fastapi/gin: 测试Gin服务")
     result: Dict[str, Any] = await call_remote_service(
         service_name="gin",
         path="/api_gin/gin",
@@ -50,10 +44,8 @@ async def testGin() -> JSONResponse:
 
 # 测试NestJS服务
 @router.get("/nestjs")
+@log("测试NestJS服务")
 async def testNestJS() -> JSONResponse:
-    user_id: str = get_current_user_id() or ""
-    username: str = get_current_username() or ""
-    fileLogger.info("用户" + user_id + ":" + username + " GET /api_fastapi/nestjs: 测试NestJS服务")
     result: Dict[str, Any] = await call_remote_service(
         service_name="nestjs",
         path="/api_nestjs/nestjs",
@@ -64,10 +56,8 @@ async def testNestJS() -> JSONResponse:
 
 # 调用定时任务（导出文章表到csv并同步hive）
 @router.post("/task")
+@log("手动触发文章表导出任务")
 async def test_export_articles_task() -> JSONResponse:
-    user_id: str = get_current_user_id() or ""
-    username: str = get_current_username() or ""
-    fileLogger.info(f"用户{user_id}:{username} GET /api_fastapi/task: 手动触发文章表导出任务")
     try:
         export_articles_to_csv_and_hive()
         return success()
