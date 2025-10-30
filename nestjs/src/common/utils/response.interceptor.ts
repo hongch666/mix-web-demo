a135228b6cafe1efafb6fe2cba0ae94bba1cbc25
hongch666 <3070872194@qq.com>
2025-10-30 22:59:32 +0800

    NestJS部分通用返回格式管道处理增加格式判断

--- nestjs/src/common/utils/response.interceptor.ts
+++ nestjs/src/common/utils/response.interceptor.ts
@@ -5,11 +5,36 @@ import {
   CallHandler,
 } from '@nestjs/common';
 import { Observable, map } from 'rxjs';
-import { success } from './response';
+import { success, ApiResponse } from './response';
 
 @Injectable()
 export class ResponseInterceptor<T> implements NestInterceptor<T, any> {
+  /**
+   * 判断是否已经是标准的 ApiResponse 格式
+   * @param data 返回的数据
+   * @returns 是否已经是标准格式
+   */
+  private isApiResponse(data: any): data is ApiResponse {
+    return (
+      data &&
+      typeof data === 'object' &&
+      'code' in data &&
+      'msg' in data &&
+      typeof data.code === 'number'
+    );
+  }
+
   intercept(context: ExecutionContext, next: CallHandler<T>): Observable<any> {
-    return next.handle().pipe(map((data) => success(data)));
+    return next.handle().pipe(
+      map((data) => {
+        // 如果已经是 ApiResponse 格式（success 或 error 返回的对象），直接返回
+        if (this.isApiResponse(data)) {
+          return data;
+        }
+
+        // 否则自动包装成 success 格式
+        return success(data);
+      }),
+    );
   }
 }
