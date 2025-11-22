ac57101604cd3aa64ffb8242b9da2a7981ce09ba
hongch666 <3070872194@qq.com>
2025-11-23 01:22:46 +0800

    FastAPI部分修改ai历史记录实体类类型

--- fastapi/entity/po/aiHistory.py
+++ fastapi/entity/po/aiHistory.py
@@ -1,15 +1,15 @@
 from sqlmodel import SQLModel, Field
 from typing import Optional
 from datetime import datetime
-from sqlalchemy import Text
+from sqlalchemy import Column, Text
 
 class AiHistory(SQLModel, table=True):
     __tablename__ = "ai_history"
     id: Optional[int] = Field(default=None, primary_key=True, index=True)
     user_id: Optional[int] = None
-    ask: str = Field(default="", sa_column_kwargs={"type": Text})
-    reply: str = Field(default="", sa_column_kwargs={"type": Text})
-    thinking: Optional[str] = Field(default=None, sa_column_kwargs={"type": Text})
+    ask: str = Field(default="", sa_column=Column(Text))
+    reply: str = Field(default="", sa_column=Column(Text))
+    thinking: Optional[str] = Field(default=None, sa_column=Column(Text))
     ai_type: str
     created_at: Optional[datetime] = Field(default_factory=datetime.now)
     updated_at: Optional[datetime] = Field(default_factory=datetime.now)
