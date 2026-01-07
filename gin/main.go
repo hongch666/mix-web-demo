4fecd1262dc3e54f126163760b31cce87826840d
hongch666 <3070872194@qq.com>
2026-01-07 22:46:00 +0800

    Gin部分修改ip和port的变量

--- gin/main.go
+++ gin/main.go
@@ -22,10 +22,12 @@ func main() {
 	// 初始化路由
 	r := api.SetupRouter()
 	// 输出启动信息和Swagger地址
-	addr := fmt.Sprintf(":%d", config.Config.Server.Port)
+	ip := config.Config.Server.Ip
+	port := config.Config.Server.Port
+	addr := fmt.Sprintf(":%d", port)
 	log.Printf("Gin应用已启动")
-	log.Printf("服务地址: http://%s:%d", config.Config.Server.Ip, config.Config.Server.Port)
-	log.Printf("Swagger文档地址: http://%s:%d/swagger/index.html", config.Config.Server.Ip, config.Config.Server.Port)
+	log.Printf("服务地址: http://%s:%d", ip, port)
+	log.Printf("Swagger文档地址: http://%s:%d/swagger/index.html", ip, port)
 	// 启动服务器
 	r.Run(addr)
 }
