9aac0b8cfb18013344dc9f7d865f55a927403fbc
hongch666 <3070872194@qq.com>
2026-01-11 22:35:26 +0800

    Gin部分修改注释

--- gin/main.go
+++ gin/main.go
@@ -18,10 +18,11 @@ import (
 func main() {
 	// 初始化路由
 	r := api.SetupRouter()
-	// 输出启动信息和Swagger地址
+	// 获取服务信息
 	ip := config.Config.Server.Ip
 	port := config.Config.Server.Port
 	addr := fmt.Sprintf(":%d", port)
+	// 输出启动信息和Swagger地址
 	log.Printf("Gin应用已启动")
 	log.Printf("服务地址: http://%s:%d", ip, port)
 	log.Printf("Swagger文档地址: http://%s:%d/swagger/index.html", ip, port)
