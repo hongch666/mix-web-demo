6d03c64bd0cc6d99c1a6ae5c34ea02f99c50d103
hongch666 <3070872194@qq.com>
2026-01-22 23:29:21 +0800

    Gin部分优化Gin常量管理，添加注释以提高可读性

--- gin/common/utils/constants.go
+++ gin/common/utils/constants.go
@@ -1,8 +1,10 @@
 package utils
 
-// Gin 自己的测试欢迎信息
 const (
+	// Gin 自己的测试欢迎信息
 	TEST_MESSAGE = "Hello,I am Gin!"
-	SEARCH_ERR   = "文章搜索错误"
-	SEARCH_MSG   = "发起了文章搜索"
+	// 文章搜索错误信息
+	SEARCH_ERR = "文章搜索错误"
+	// 文字搜索消息
+	SEARCH_MSG = "发起了文章搜索"
 )
