9aca9093d13f1462633a9ef8ad9d01d0da557821
hongch666 <3070872194@qq.com>
2026-01-24 01:14:31 +0800

    NestJS部分优化ClsMiddleware中的请求头获取，统一使用小写格式适配Fastify

--- nestjs/src/common/middleware/cls.middleware.ts
+++ nestjs/src/common/middleware/cls.middleware.ts
@@ -7,9 +7,9 @@ export class ClsMiddleware implements NestMiddleware {
   constructor(private readonly cls: ClsService) {}
 
   use(req: FastifyRequest, res: FastifyReply, next: () => void) {
-    // userId 在请求头中，这里使用 X-User-Id
-    const userId = req.headers['X-User-Id'];
-    const username = req.headers['X-Username'];
+    // userId 在请求头中，这里使用 x-user-id（小写），由于 Fastify 会将所有头部转换为小写
+    const userId = req.headers['x-user-id'];
+    const username = req.headers['x-username'];
     this.cls.set('userId', userId);
     this.cls.set('username', username);
     next();
