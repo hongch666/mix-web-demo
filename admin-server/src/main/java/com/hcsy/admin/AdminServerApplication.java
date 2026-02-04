0018d4d7b7d3d364bae6145b5092ed7c7864511a
hongch666 <3070872194@qq.com>
2026-02-04 13:32:20 +0800

    增加SpringBoot Admin的服务端, 在spring服务封装客户端

---
+++ admin-server/src/main/java/com/hcsy/admin/AdminServerApplication.java
@@ -0,0 +1,17 @@
+package com.hcsy.admin;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Spring Boot Admin Server 启动类
+ */
+@SpringBootApplication
+@EnableAdminServer
+public class AdminServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdminServerApplication.class, args);
+    }
+}
