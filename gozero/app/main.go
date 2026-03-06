f61451ee82210cf8786f9192724f7f72af4505d5
hongch666 <3070872194@qq.com>
2026-03-06 20:19:56 +0800

    GoZero部分修改注释

--- gozero/app/main.go
+++ gozero/app/main.go
@@ -11,11 +11,11 @@ var configFile = flag.String("f", "etc/application.yaml", "the config file")
 
 func main() {
 	flag.Parse()
-	// 加载配置（包含环境变量）
+	// 加载配置
 	cfg := boot.LoadConfig(*configFile)
 	// 创建服务上下文
 	ctx := svc.NewServiceContext(cfg)
-	// 创建并初始化服务器（包含路由注册、Swagger、任务调度器、启动信息输出）
+	// 创建并初始化服务器
 	server := boot.CreateServer(cfg, ctx)
 	defer server.Stop()
 	// 启动服务器
