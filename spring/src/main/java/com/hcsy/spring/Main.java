dbbead56b881f283811f8703c86f627ec85c7e75
hongch666 <3070872194@qq.com>
2026-03-17 10:39:13 +0800

    Spring部分添加DotenvInitializer类以加载.env文件中的环境变量，优化应用启动前的环境配置

--- spring/src/main/java/com/hcsy/spring/Main.java
+++ spring/src/main/java/com/hcsy/spring/Main.java
@@ -2,11 +2,14 @@ package com.hcsy.spring;
 
 import org.springframework.boot.SpringApplication;
 import com.hcsy.spring.core.annotation.Starter;
+import com.hcsy.spring.infra.initializer.DotenvInitializer;
 
 @Starter
 public class Main {
 
     public static void main(String[] args) {
+        // 在应用启动前加载.env文件中的环境变量
+        DotenvInitializer.loadEnv();
         // 启动 Spring Boot 应用
         SpringApplication.run(Main.class, args);
     }
