2ae8bcd6831d35e3519136ba710f9feae4e7d272
hongch666 <3070872194@qq.com>
2026-03-29 02:18:33 +0800

    GoZero为各API添加中间件配置，优化路由处理

--- gozero/app/internal/handler/routes.go
+++ gozero/app/internal/handler/routes.go
@@ -18,126 +18,131 @@ func RegisterHandlers(server *rest.Server, serverCtx *svc.ServiceContext) {
 	server.AddRoutes(
 		rest.WithMiddlewares(
 			[]rest.Middleware{serverCtx.UserContextMiddleware, serverCtx.RecoveryMiddleware},
-			[]rest.Route{}...,
+			[]rest.Route{
+				{
+					// SSE消息通知连接
+					Method:  http.MethodGet,
+					Path:    "/sse/chat",
+					Handler: chat.ChatSSEHandler(serverCtx),
+				},
+				{
+					// WebSocket聊天连接
+					Method:  http.MethodGet,
+					Path:    "/ws/chat",
+					Handler: chat.ChatWebsocketHandler(serverCtx),
+				},
+			}...,
 		),
 	)
 
 	server.AddRoutes(
-		[]rest.Route{
-			{
-				// 获取用户的所有未读消息数
-				Method:  http.MethodPost,
-				Path:    "/all-unread-counts",
-				Handler: chat.ChatGetAllUnreadCountsHandler(serverCtx),
-			},
-			{
-				// 获取聊天历史
-				Method:  http.MethodPost,
-				Path:    "/history",
-				Handler: chat.ChatGetHistoryHandler(serverCtx),
-			},
-			{
-				// 手动加入聊天队列
-				Method:  http.MethodPost,
-				Path:    "/join",
-				Handler: chat.ChatJoinQueueHandler(serverCtx),
-			},
-			{
-				// 手动离开聊天队列
-				Method:  http.MethodPost,
-				Path:    "/leave",
-				Handler: chat.ChatLeaveQueueHandler(serverCtx),
-			},
-			{
-				// 获取队列状态
-				Method:  http.MethodGet,
-				Path:    "/queue",
-				Handler: chat.ChatGetQueueStatusHandler(serverCtx),
-			},
-			{
-				// 发送聊天消息
-				Method:  http.MethodPost,
-				Path:    "/send",
-				Handler: chat.ChatSendMessageHandler(serverCtx),
-			},
-			{
-				// 获取两个用户间的未读消息数
-				Method:  http.MethodPost,
-				Path:    "/unread-count",
-				Handler: chat.ChatGetUnreadCountHandler(serverCtx),
-			},
-		},
+		rest.WithMiddlewares(
+			[]rest.Middleware{serverCtx.UserContextMiddleware, serverCtx.RecoveryMiddleware},
+			[]rest.Route{
+				{
+					// 获取用户的所有未读消息数
+					Method:  http.MethodPost,
+					Path:    "/all-unread-counts",
+					Handler: chat.ChatGetAllUnreadCountsHandler(serverCtx),
+				},
+				{
+					// 获取聊天历史
+					Method:  http.MethodPost,
+					Path:    "/history",
+					Handler: chat.ChatGetHistoryHandler(serverCtx),
+				},
+				{
+					// 手动加入聊天队列
+					Method:  http.MethodPost,
+					Path:    "/join",
+					Handler: chat.ChatJoinQueueHandler(serverCtx),
+				},
+				{
+					// 手动离开聊天队列
+					Method:  http.MethodPost,
+					Path:    "/leave",
+					Handler: chat.ChatLeaveQueueHandler(serverCtx),
+				},
+				{
+					// 获取队列状态
+					Method:  http.MethodGet,
+					Path:    "/queue",
+					Handler: chat.ChatGetQueueStatusHandler(serverCtx),
+				},
+				{
+					// 发送聊天消息
+					Method:  http.MethodPost,
+					Path:    "/send",
+					Handler: chat.ChatSendMessageHandler(serverCtx),
+				},
+				{
+					// 获取两个用户间的未读消息数
+					Method:  http.MethodPost,
+					Path:    "/unread-count",
+					Handler: chat.ChatGetUnreadCountHandler(serverCtx),
+				},
+			}...,
+		),
 		rest.WithPrefix("/user-chat"),
 	)
 
 	server.AddRoutes(
-		[]rest.Route{
-			{
-				// SSE消息通知连接
-				Method:  http.MethodGet,
-				Path:    "/sse/chat",
-				Handler: chat.ChatSSEHandler(serverCtx),
-			},
-			{
-				// WebSocket聊天连接
-				Method:  http.MethodGet,
-				Path:    "/ws/chat",
-				Handler: chat.ChatWebsocketHandler(serverCtx),
-			},
-		},
-	)
-
-	server.AddRoutes(
-		[]rest.Route{
-			{
-				// 搜索文章
-				Method:  http.MethodGet,
-				Path:    "/",
-				Handler: search.SearchArticlesHandler(serverCtx),
-			},
-			{
-				// 获取用户搜索历史
-				Method:  http.MethodGet,
-				Path:    "/history/:userId",
-				Handler: search.GetSearchHistoryHandler(serverCtx),
-			},
-		},
+		rest.WithMiddlewares(
+			[]rest.Middleware{serverCtx.UserContextMiddleware, serverCtx.RecoveryMiddleware},
+			[]rest.Route{
+				{
+					// 搜索文章
+					Method:  http.MethodGet,
+					Path:    "/",
+					Handler: search.SearchArticlesHandler(serverCtx),
+				},
+				{
+					// 获取用户搜索历史
+					Method:  http.MethodGet,
+					Path:    "/history/:userId",
+					Handler: search.GetSearchHistoryHandler(serverCtx),
+				},
+			}...,
+		),
 		rest.WithPrefix("/search"),
 	)
 
 	server.AddRoutes(
-		[]rest.Route{
-			{
-				// 调用FastAPI的测试
-				Method:  http.MethodGet,
-				Path:    "/fastapi",
-				Handler: test.TestFastAPIHandler(serverCtx),
-			},
-			{
-				// 测试GoZero服务
-				Method:  http.MethodGet,
-				Path:    "/gozero",
-				Handler: test.TestGoZeroHandler(serverCtx),
-			},
-			{
-				// 调用NestJS的测试
-				Method:  http.MethodGet,
-				Path:    "/nestjs",
-				Handler: test.TestNestJSHandler(serverCtx),
-			},
-			{
-				// 调用Spring的测试
-				Method:  http.MethodGet,
-				Path:    "/spring",
-				Handler: test.TestSpringHandler(serverCtx),
-			},
-		},
+		rest.WithMiddlewares(
+			[]rest.Middleware{serverCtx.UserContextMiddleware, serverCtx.RecoveryMiddleware},
+			[]rest.Route{
+				{
+					// 调用FastAPI的测试
+					Method:  http.MethodGet,
+					Path:    "/fastapi",
+					Handler: test.TestFastAPIHandler(serverCtx),
+				},
+				{
+					// 测试GoZero服务
+					Method:  http.MethodGet,
+					Path:    "/gozero",
+					Handler: test.TestGoZeroHandler(serverCtx),
+				},
+				{
+					// 调用NestJS的测试
+					Method:  http.MethodGet,
+					Path:    "/nestjs",
+					Handler: test.TestNestJSHandler(serverCtx),
+				},
+				{
+					// 调用Spring的测试
+					Method:  http.MethodGet,
+					Path:    "/spring",
+					Handler: test.TestSpringHandler(serverCtx),
+				},
+			}...,
+		),
 		rest.WithPrefix("/api_gozero"),
 	)
 
 	server.AddRoutes(
 		rest.WithMiddlewares(
-			[]rest.Middleware{serverCtx.InternalServiceMiddleware},
+			[]rest.Middleware{serverCtx.UserContextMiddleware, serverCtx.RecoveryMiddleware, serverCtx.InternalServiceMiddleware},
 			[]rest.Route{
 				{
 					// 调用同步ES的测试
