296c71e40087903381b14b93ca8e9eb42e5c0a88
hongch666 <3070872194@qq.com>
2026-07-20 21:17:06 +0800

    NestJS部分调整SystemModule的导入顺序，确保ArticleLogModule在ArticleModule之前导入，并更新exports以包含所有模块

--- nestjs/src/module/system/system.module.ts
+++ nestjs/src/module/system/system.module.ts
@@ -1,7 +1,7 @@
 import { Module } from "@nestjs/common";
 import { ApiLogModule } from "./apiLog/apiLog.module";
-import { ArticleLogModule } from "./articleLog/articleLog.module";
 import { ArticleModule } from "./article/article.module";
+import { ArticleLogModule } from "./articleLog/articleLog.module";
 import { DownloadModule } from "./download/download.module";
 import { TestModule } from "./test/test.module";
 import { UploadModule } from "./upload/upload.module";
@@ -15,6 +15,14 @@ import { UserModule } from "./user/user.module";
     DownloadModule,
     ArticleModule,
     UserModule,
+    UploadModule,
+  ],
+  exports: [
+    UserModule,
+    ArticleModule,
+    ArticleLogModule,
+    ApiLogModule,
+    DownloadModule,
     UploadModule,
   ],
 })
