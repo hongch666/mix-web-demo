9b31e7846d0d7f9358acb572769d3cd3a536388e
Shichao Chu <129023404+hongch666@users.noreply.github.com>
2026-09-14 18:09:53 +0800

    NestJS部分优化CommonModule，移除未使用的模块和导出，简化依赖关系

--- nestjs/src/module/common/common.module.ts
+++ nestjs/src/module/common/common.module.ts
@@ -1,33 +1,12 @@
 import { Module } from "@nestjs/common";
 import { ClientModule } from "./client/client.module";
 import { GithubModule } from "./github/github.module";
-import { MailModule } from "./mail/mail.module";
 import { LoggerModule } from "./logger/logger.module";
-import { NacosModule } from "./nacos/nacos.module";
-import { OssModule } from "./oss/oss.module";
+import { MailModule } from "./mail/mail.module";
 import { TaskModule } from "./task/task.module";
-import { WordModule } from "./word/word.module";
 
 @Module({
-  imports: [
-    ClientModule,
-    GithubModule,
-    LoggerModule,
-    MailModule,
-    NacosModule,
-    TaskModule,
-    WordModule,
-    OssModule,
-  ],
-  exports: [
-    ClientModule,
-    GithubModule,
-    LoggerModule,
-    MailModule,
-    NacosModule,
-    TaskModule,
-    WordModule,
-    OssModule,
-  ],
+  imports: [LoggerModule, ClientModule, GithubModule, MailModule, TaskModule],
+  exports: [ClientModule],
 })
 export class CommonModule {}
